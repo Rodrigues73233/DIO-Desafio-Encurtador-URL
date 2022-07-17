41ee7550d4246eb8bbb9dd582ea1d005452c372a
Gabriel Rodrigues <102063081+Rodrigues73233@users.noreply.github.com>
2022-07-17 14:24:09 -0300

    Add files via upload

---
+++ src/config/Constants.ts
@@ -0,0 +1,5 @@
+export const config = {
+	API_URL: 'http://localhost:5000',
+	MONGO_CONNECTION:
+		'mongodb+srv://Gabriel:24324433@cluster0.z5ys1qv.mongodb.net/Cluster0?retryWrites=true&w=majority',
+}
